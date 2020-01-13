1eaa8f830cebda6af9a547a056c5c2b5b5db30c3
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-01-12 20:13:52 -0500

    Adding Menu Component

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Menu;
