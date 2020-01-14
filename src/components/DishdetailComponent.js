a2f5b79ba5c9bdd5331d0677a79268f2577fbf59
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-01-13 20:01:42 -0500

    Task #1, Task #2 y Task #3 finished

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -21,11 +21,34 @@ class DishDetail extends Component {
         }   
     }
 
+    getComments(dish) {
+        if(dish != null) {
+            return dish.comments;
+        }
+    }
+
+    convertDate(date) {
+        const oldDate = new Date(date);
+        const newDate = oldDate.toString().split(" ");
+        const finalDate = newDate[1] + " " + (parseInt(newDate[2]) + 1)  + ", " + newDate[3];
+        return finalDate;
+    }
+
     renderComments(comments){
         if(comments != null){
+            const items = comments.map((item) => {
+                return(
+                    <ul key={ item.id } className="list-unstyled">
+                        <li>{ item.comment }</li>
+                        <br />
+                        <li>-- { item.author } , { this.convertDate(item.date) }</li>
+                    </ul>
+                );
+            });
             return(
                 <div>
-                
+                    <h4>Comments</h4>
+                    { items }
                 </div>
             )
         } else {
@@ -36,15 +59,14 @@ class DishDetail extends Component {
     }
     
     render(){
-        console.log(this.props.selectedDish)
+        const comments = this.getComments(this.props.selectedDish);
         return(
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     { this.renderDish(this.props.selectedDish) }
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    { this.renderComments(this.props.selectedDish) }
+                    { this.renderComments(comments) }
                 </div>
             </div>
         )
