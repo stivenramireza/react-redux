0061598343e4f8c37fc98de4dfc4ca84909a8eb2
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-01-18 18:50:39 -0500

    Adding Header Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import { DISHES } from '../shared/dishes';
 import Menu from '../components/MenuComponent';
 import DishDetail from '../components/DishdetailComponent';
+import Header from '../components/HeaderComponent';
 
 class Main extends Component {
 
@@ -23,12 +23,8 @@ class Main extends Component {
     render(){
         return(
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                    </div>
-                </Navbar>
-                <Menu dishes={this.state.dishes} onClick={ (dishId) => this.onDishSelect(dishId) } />
+                <Header />
+                <Menu dishes={this.state.dishes} onClick={ (dishId) => this.onDishSelect(   ) } />
                 <DishDetail selectedDish={this.state.dishes.filter( (dish) => dish.id === this.state.selectedDish)[0]} />
             </div>
         )
