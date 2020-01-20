c2b35e9152c2b7e176b1b761507bff6f9fbca864
Stiven Ramírez Arango <sramirez@anutibara.com>
2020-01-20 16:47:55 -0500

    Task #1 finished from Assignment #2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function About(props) {
+const About = (props) => {
 
     const leaders = props.leaders.map((leader) => {
         return (
